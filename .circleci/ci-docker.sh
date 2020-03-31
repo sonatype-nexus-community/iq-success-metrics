3c1ff1834ffdb05df9fd2101eea122badac3cadb
Jeffry Hesse <jhesse@sonatype.com>
2020-03-30 19:41:48 -0800

    echo in password

--- .circleci/ci-docker.sh
+++ .circleci/ci-docker.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
+echo "$DOCKER_PASSWORD" | docker login -u $DOCKER_USERNAME --password-stdin
 docker build -t sonatypecommunity/iq-success-metrics:$CIRCLE_TAG .
 docker tag sonatypecommunity/iq-success-metrics:$CIRCLE_TAG sonatypecommunity/iq-success-metrics:latest
 docker push sonatypecommunity/iq-success-metrics
